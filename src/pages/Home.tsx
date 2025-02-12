c9096ed9086c526d75e6aad559c65bfdb46cc5ca
Zachary <duvallzt@gmail.com>
2025-02-11 21:34:35 -0500

    Update home

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -13,16 +13,14 @@ const Home = () => {
 
   return (
     <div className='container mx-auto p-4'>
-      {/* Container and padding */}
       <h1 className='text-3xl font-bold mb-4'>Welcome to Words of Wisdom</h1>
-      {/* Title styling */}
-      <QuoteCard data={quotes[index]} reveal={true} />
       <button
-        className='accent-background-hover text-white font-bold py-2 px-4 rounded mt-4 cursor-pointer' // Button styling
+        className='accent-background-hover text-white font-bold py-2 px-4 rounded mb-4 cursor-pointer' // Button styling
         onClick={newRandomQuote}
       >
         New Random Quote
       </button>
+      <QuoteCard data={quotes[index]} reveal={true} />
     </div>
   );
 };
