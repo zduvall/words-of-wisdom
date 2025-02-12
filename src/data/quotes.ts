7a29e752c39150b7db668ee36d373e6050b804ec
Zachary <duvallzt@gmail.com>
2025-02-11 20:01:16 -0500

    Update some quotes

--- src/data/quotes.ts
+++ src/data/quotes.ts
@@ -145,6 +145,13 @@ const quotesData: Quote[] = [
     source: 'Harvey, 1950',
     hint: 'Always a wonderful time',
   },
+  {
+    quote:
+      'As each situation in life represents a challenge to man and presents a problem for him to solve, the question of the meaning of life may actually be reversed. Ultimately, man should not ask what the meaning of his life is, but rather he must recognize that it is he who is asked. In a word, each man is questioned by life; and he can only answer to life by answering for his own life; to life he can only respond by being responsible.',
+    author: 'Viktor Frankl',
+    source: "Man's Search for Meaning, 1946",
+    hint: 'Answering for his own life',
+  },
 ];
 
 export default quotesData;
