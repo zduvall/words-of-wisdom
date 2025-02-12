13883caa274d3fa2e117079fd396436f72030bb4
Zachary <duvallzt@gmail.com>
2025-02-11 19:16:33 -0500

    Update title

--- src/pages/Quote.tsx
+++ src/pages/Quote.tsx
@@ -41,9 +41,11 @@ const Quote = ({ shuffle = false, testMode = false }: ITestProps) => {
     setRevealed(!testMode);
   };
 
+  const title = shuffle ? 'Test Your Memory' : 'Quote';
+
   return (
     <div className='container mx-auto p-4'>
-      <h1 className='text-3xl font-bold mb-4'>Test Your Memory</h1>
+      <h1 className='text-3xl font-bold mb-4'>{title}</h1>
       {curQuote && (
         <QuoteCard
           data={curQuote}
