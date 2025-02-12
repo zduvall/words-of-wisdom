ebb299b5b1f17432a399fabb8712b34cd60e5677
Zachary <duvallzt@gmail.com>
2025-02-11 19:25:41 -0500

    Update some quotes

--- src/components/QuoteCard.tsx
+++ src/components/QuoteCard.tsx
@@ -23,7 +23,7 @@ const QuoteCard = ({ data, reveal = true, onToggle }: IQuoteCardProps) => {
     <div className={classNames} onClick={onToggle}>
       {reveal ? (
         <>
-          <p className='text-lg mb-2'>“{data.quote}”</p>
+          <p className='text-lg mb-2 whitespace-pre-line'>“{data.quote}”</p>
           <div className='flex gap-2'>
             <p className='text-base italic mb-4'>— {data.author},</p>
             <p className='text-base italic mb-2'>{data.source}</p>
