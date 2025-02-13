aca449822bf95cec46dc2b4d28ec05e745cceaed
Zachary <duvallzt@gmail.com>
2025-02-13 18:05:02 -0500

    Add another quote from Mr Rogers

--- src/data/quotes.ts
+++ src/data/quotes.ts
@@ -159,11 +159,19 @@ const quotesDataPartial: IQuotePartial[] = [
   },
   {
     quote:
-      '...ye cannot in your present state understand eternity… But ye can get some likeness of it if ye say that both good and evil, when they are full grown, become retrospective. Not only this valley but all their earthly past will have been Heaven to those who are saved. Not only the twilight in that town, but all their life on Earth too, will then be seen by the damned to have been Hell. That is what mortals misunderstand. They say of some temporal suffering, "No future bliss can make up for it," not knowing that Heaven, once attained, will work backwards and turn even that agony into a glory. And of some sinful pleasure they say "Let me have but this and I\'ll take the consequences": little dreaming how damnation will spread back and back into their past and contaminate the pleasure of the sin. Both processes begin even before death. The good man\'s past begins to change so that his forgiven sins and remembered sorrows take on the quality of Heaven: the bad man\'s past already conforms to his badness and is filled only with dreariness. And that is why, at the end of all things, when the sun rises here and the twilight turns to blackness down there, the Blessed will say "We have never lived anywhere except in Heaven," and the Lost, "We were always in Hell." And both will speak truly.',
-    author: 'C.S. Lewis',
-    source: 'The Great Divorce, 1945',
-    hint: 'Eternity is retrospective',
+      'Listening is where love begins: listening to ourselves and then to our neighbors.',
+    author: 'Fred Rogers',
+    source:
+      'The World According to Mister Rogers: Important Things to Remember, 2003',
+    hint: 'Where love begins',
   },
+  // { // commented out b/c is really long
+  //   quote:
+  //     '...ye cannot in your present state understand eternity… But ye can get some likeness of it if ye say that both good and evil, when they are full grown, become retrospective. Not only this valley but all their earthly past will have been Heaven to those who are saved. Not only the twilight in that town, but all their life on Earth too, will then be seen by the damned to have been Hell. That is what mortals misunderstand. They say of some temporal suffering, "No future bliss can make up for it," not knowing that Heaven, once attained, will work backwards and turn even that agony into a glory. And of some sinful pleasure they say "Let me have but this and I\'ll take the consequences": little dreaming how damnation will spread back and back into their past and contaminate the pleasure of the sin. Both processes begin even before death. The good man\'s past begins to change so that his forgiven sins and remembered sorrows take on the quality of Heaven: the bad man\'s past already conforms to his badness and is filled only with dreariness. And that is why, at the end of all things, when the sun rises here and the twilight turns to blackness down there, the Blessed will say "We have never lived anywhere except in Heaven," and the Lost, "We were always in Hell." And both will speak truly.',
+  //   author: 'C.S. Lewis',
+  //   source: 'The Great Divorce, 1945',
+  //   hint: 'Eternity is retrospective',
+  // },
 ];
 
 const quotesData: IQuote[] = quotesDataPartial.map((quote, idx) => ({
